6664ae39e5f457010bf0c93bc6eb15a89a65a2ec
reisub0 <reisub0@gmail.com>
2018-06-26 15:17:54 +0530

    Added requests post api
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- gitlab-init.py
+++ gitlab-init.py
@@ -12,7 +12,7 @@ from sys import argv
 import sh
 import os
 from sh.contrib import git
-import requests
+import requests as r
 
 repoName = argv[1]
 
@@ -20,13 +20,18 @@ repoName = argv[1]
 if not os.path.exists(os.path.join(os.getcwd(), '.git')):
     git.init()
 
+with open(os.path.expanduser('~/.local/.gitlabtoken'), 'r') as f:
+    gitlabAuth = f.read()
+
 try:
-    sh.lab('project', 'create', repoName)
-except:
+    # sh.lab('project', 'create', repoName)
+    response = r.post('http://gitlab.com', json={"name": repoName, "visibility": "public"})
+    r.raise_for_status()
+except Exception:
     print('Something went wrong with creating the GitLab repo, Already exists?')
+    print(Exception)
+
 try:
     os.system('hub create')
 except:
     print('Something went wrong with creating the GitHub repo, Already exists?')
-# sh.echo('hi')
-# def githubPublish(name,):
